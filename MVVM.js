80db4cecdb1d506ede912c6dfd84fb6d10b3ff97
Steve <ycsteven.tsao@gmail.com>
2020-05-18 11:31:44 +0800

    feat: proxy
    內容：原本用 vm.$data.message 取值，改成用 vm.message 取值

--- MVVM.js
+++ MVVM.js
@@ -8,8 +8,21 @@ class MVVM {
     if (this.$el) {
       // 數據攔截，把物件的所有屬性改成 get 和 set 方法
       new Observer(this.$data);
+      this.proxyData(this.$data);
       // 用數據和元素進行編譯
       new Compile(this.$el, this);
     }
+  }
+  proxyData(data) {
+    Object.keys(data).forEach((key) => {
+      Object.defineProperty(this, key, {
+        get() {
+          return data[key];
+        },
+        set(newValue) {
+          data[key] = newValue;
+        },
+      });
+    });
   }
 }
